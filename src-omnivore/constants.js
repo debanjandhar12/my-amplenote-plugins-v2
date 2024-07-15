a62b813aefc331a7f431fc98caf0488197824df9
debanjandhar12 <debanjandhar12@gmail.com>
2024-07-15 12:14:07 +0530

    omnivore: setting name change

--- src-omnivore/constants.js
+++ src-omnivore/constants.js
@@ -8,7 +8,7 @@ export const OMNIVORE_DASHBOARD_COLUMNS_SETTING_DEFAULT = 'Author, UpdatedAt';
 // Setting Titles
 export const OMNIVORE_DASHBOARD_NOTE_TITLE_SETTING = `Omnivore dashboard note title (default: "${OMNIVORE_DASHBOARD_NOTE_TITLE_DEFAULT}")`;
 export const OMNIVORE_DASHBOARD_ORDER_SETTING = `Omnivore dashboard order ("updatedAt-asc" or "updatedAt-desc" or "savedAt-asc" or "savedAt-desc"). Default: "${OMNIVORE_DASHBOARD_ORDER_SETTING_DEFAULT}")`;
-export const OMNIVORE_DASHBOARD_COLUMNS_SETTING = 'Omnivore dashboard columns (comma separated list of: "Author", "Description", "UpdatedAt", "SavedAt", "PageType", "ReadingProgressPercent")';
+export const OMNIVORE_DASHBOARD_COLUMNS_SETTING = `Omnivore dashboard columns (comma separated list of: "Author", "Description", "UpdatedAt", "SavedAt", "PageType", "ReadingProgressPercent") (default: "${OMNIVORE_DASHBOARD_COLUMNS_SETTING_DEFAULT}")`;
 export const BASE_TAG_FOR_HIGHLIGHT_NOTE_SETTING = `Base tag for highlight note (default: "${BASE_TAG_FOR_HIGHLIGHT_NOTE_SETTING_DEFAULT}")`;
 export const NOTE_HIGHLIGHT_ORDER_SETTING = `Highlight order ("updatedAt-asc" or "updatedAt-desc". Default: "${NOTE_HIGHLIGHT_ORDER_SETTING_DEFAULT}")`;
 
