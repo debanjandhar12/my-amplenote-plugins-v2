b6948fb24a4687819264f27c42e6cef9d50ef8c3
debanjandhar12 <debanjandhar12@gmail.com>
2024-08-06 20:27:00 +0530

    mindmap(chore): update about page

--- src-mindmap/plugin.about.js
+++ src-mindmap/plugin.about.js
@@ -10,14 +10,17 @@ export default {
     description: 'Visualize your markdown notes in a mindmap',
     settings: [TITLE_AS_DEFAULT_NODE_SETTING, INITIAL_EXPAND_LEVEL_SETTING, SHOW_ONLY_SIBLINGS_AT_CURRENT_LEVEL_SETTING],
     version: MINDMAP_PLUGIN_VERSION,
-    icon: 'sync_alt',
+    icon: 'account_tree',
     instructions: `
 Visualize your markdown notes in a mindmap.
 ![](https://images.amplenote.com/073fd84a-46af-11ef-bbe6-26e37c279344/6cc8817a-f183-4899-a8ab-af980ff76101.gif)
 To use this plugin, select "Preview mindmap" from the note menu.
+
+**Features:**
+- Preview your notes as a Mind Map
+- Mind Map preview updates on clicking reload toolbar button.
 `.trim().replaceAll('\n', '<br />'),
     template: `
-> ⚠️ This is a beta version of the plugin. Please report any issues to the developer.    
     
 ### Code
 <<Code>>
@@ -25,5 +28,6 @@ To use this plugin, select "Preview mindmap" from the note menu.
 
 ### Changelog
 20/07/2024 - First version
+06/08/2024 - Bug fixes
 `.trim()
 };
