4ca4f14bf846a9e43a0578946f4e6cacc4d266d2
debanjandhar12 <debanjandhar12@gmail.com>
2024-09-14 16:26:01 +0530

    enhance(autolink): improve tests

--- src-autolink/test/removeLinksFromMarkdown.test.js
+++
@@ -1,9 +0,0 @@
-import {removeLinksFromMarkdown} from "../core/removeLinksFromMarkdown.js";
-
-describe('removeLinksFromMarkdown', () => {
-    it('should remove all links from the markdown text', async () => {
-        const markdownText = 'This is a [JavaScript](https://example.com) note.';
-        const result = await removeLinksFromMarkdown(markdownText);
-        expect(result).toBe('This is a JavaScript note.');
-    });
-});
