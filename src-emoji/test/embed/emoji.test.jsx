fe3758e70f7783d6b8c8ae95cb77a52ef30cb9f9
debanjandhar12 <debanjandhar12@gmail.com>
2024-10-02 14:10:22 +0530

    fix: not working

--- src-emoji/test/embed/emoji.test.jsx
+++ src-emoji/test/embed/emoji.test.jsx
@@ -1,10 +1,11 @@
 import {render, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
-import dynamicImportESM from "../../common-utils/dynamic-import-esm.js";
+import dynamicImportESM from "../../../common-utils/dynamic-import-esm.js";
+import {App} from "../../embed/emoji.jsx";
 
 test('renders app', async () => {
     window.React = await dynamicImportESM("react");
     window.ReactDOM = await dynamicImportESM("react-dom");
-    render(<div>Hello World</div>)
+    render(<App />)
     expect(screen.getByText('Hello World')).toBeInTheDocument()
 });
