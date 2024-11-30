dfeabdba09bf33a4f21ae9f37f2e87467b6a603b
debanjandhar12 <debanjandhar12@gmail.com>
2024-11-30 20:04:15 +0530

    copilot: improve tools ui

--- src-copilot/ai-frontend/components/ToolCardMessage.jsx
+++ src-copilot/ai-frontend/components/ToolCardMessage.jsx
@@ -1,9 +1,12 @@
 import {ToolCardContainer} from "./ToolCardContainer.jsx";
 
-export const ToolCardMessage = ({text, color = false}) => {
-    const { Text } = window.RadixUI;
+export const ToolCardMessage = ({icon, text, color = false}) => {
+    const { Text, Flex } = window.RadixUI;
 
     return <ToolCardContainer>
-        <Text color={color}>{text}</Text>
+        <Flex align="center" gap="2">
+            {icon && icon}
+            <Text color={color}>{text}</Text>
+        </Flex>
     </ToolCardContainer>
 }
