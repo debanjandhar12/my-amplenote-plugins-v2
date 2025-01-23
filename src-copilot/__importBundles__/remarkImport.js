e46f435debba5266b5a75576b2be28869daf913c
debanjandhar12 <debanjandhar12@gmail.com>
2025-01-23 19:50:09 +0530

    test

---
+++ src-copilot/__importBundles__/remarkImport.js
@@ -0,0 +1,6 @@
+import * as pkg1 from "remark-gfm";
+import * as pkg2 from "unified";
+import * as pkg3 from "remark-frontmatter";
+import * as pkg4 from "remark-parse";
+
+export default [pkg1, pkg2, pkg3, pkg4];
