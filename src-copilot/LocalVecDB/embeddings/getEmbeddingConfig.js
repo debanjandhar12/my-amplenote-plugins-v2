a25a46b882a0e1c7970c5139fc2fb5dc12a15909
debanjandhar12 <debanjandhar12@gmail.com>
2025-03-14 21:21:36 +0530

    copilot: dont use webgpu when f16 not enabled

--- src-copilot/LocalVecDB/embeddings/getEmbeddingConfig.js
+++ src-copilot/LocalVecDB/embeddings/getEmbeddingConfig.js
@@ -18,7 +18,8 @@ export async function getEmbeddingConfig(app, navigator = window.navigator) {
         if (window.gpuAdapter == null && navigator.gpu)
             window.gpuAdapter = await navigator.gpu.requestAdapter() || false;
     } catch (e) {}
-    const webGpuAvailable = window.gpuAdapter !== false && window.gpuAdapter !== null && window.gpuAdapter !== undefined;
+    const webGpuAvailable = window.gpuAdapter !== false && window.gpuAdapter !== null
+        && window.gpuAdapter !== undefined && window.gpuAdapter.features.has("shader-f16");
     return {
         provider: "local",
         model: "Snowflake/snowflake-arctic-embed-s",
