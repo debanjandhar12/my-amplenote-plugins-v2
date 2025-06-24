d8f40632f7b2cb0b9a709c0c160651a9189cd956
debanjandhar12 <debanjandhar12@gmail.com>
2025-06-24 20:32:01 +0530

    copilot: add user defined prompts to refine selection

--- src-copilot/plugin-backend/handleRefineSelection.js
+++ src-copilot/plugin-backend/handleRefineSelection.js
@@ -1,7 +1,14 @@
 import {generateText} from "../aisdk-wrappers/generateText.js";
 import {getLLMModel} from "../aisdk-wrappers/getLLMModel.js";
+import {USER_PROMPT_LIST_SETTING} from "../constants.js";
+import {truncate} from "lodash-es";
 
 export async function handleRefineSelection(app, selectionContent) {
+    let savedPromptList = [];
+    try {
+        (JSON.parse(app.settings[USER_PROMPT_LIST_SETTING])).sort((a, b) => b.usageCount - a.usageCount).forEach((prompt, index) =>
+            savedPromptList.push({ icon: "auto_fix_high", label: truncate(prompt.message, {length: 96}), fullLabel: prompt.message, value: "SavedCustomPrompt" + index }));
+    } catch (e) {}
     let promptPrefix = await app.prompt("", {
         inputs: [
             { label: "Enter prompt type:", type: "select", options: [
@@ -11,11 +18,17 @@ export async function handleRefineSelection(app, selectionContent) {
                     { icon: "work", label: "More formal", value: "Make the following selected text more formal:" },
                     { icon: "beach_access", label: "More casual", value: "Make the following selected text more casual:" },
                     { icon: "healing", label: "Fix grammar", value: "Rectify grammar and spelling in the following selected text:" },
+                    ...savedPromptList,
                     { icon: "edit", label: "Custom", value: "Custom" }
                 ], value: "Rephrase the following selected text:" }
         ]
     });
     if (!promptPrefix) return;
+    if (promptPrefix.startsWith("SavedCustomPrompt")) {
+        promptPrefix = savedPromptList[parseInt(promptPrefix.substring("SavedCustomPrompt".length))].fullLabel;
+        promptPrefix = "Follow instructions bellow to edit text:\n" + promptPrefix;
+        promptPrefix += "\nSelected text";
+    }
     if (promptPrefix === "Custom") {
         promptPrefix = await app.prompt("Enter custom prompt:");
         promptPrefix += "\nSelected text";
