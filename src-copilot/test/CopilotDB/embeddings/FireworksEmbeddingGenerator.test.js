ab64bc9cc44a14acecbf4017a3ccd40a8faa83b6
debanjandhar12 <debanjandhar12@gmail.com>
2025-07-11 22:58:25 +0530

    fix tests

--- src-copilot/test/CopilotDB/embeddings/FireworksEmbeddingGenerator.test.js
+++ src-copilot/test/CopilotDB/embeddings/FireworksEmbeddingGenerator.test.js
@@ -4,26 +4,24 @@ import {EMBEDDING_API_KEY_SETTING} from "../../../constants.js";
 
 describe('Fireworks Embedding', () => {
     test('works with single string', async () => {
-        if (!process.env.FIREWORKS_API_KEY) return;
         const embeddingGenerator = new FireworksEmbeddingGenerator();
         const app = mockApp();
         app.settings[EMBEDDING_API_KEY_SETTING] = process.env.FIREWORKS_API_KEY;
         const result = await embeddingGenerator.generateEmbedding(app, "Hi", 'query');
         expect(result.length).toBe(1);
         expect(Array.isArray(result)).toBe(true);
-        expect(Array.isArray(result[0])).toBe(true);
+        expect(result[0] instanceof Float32Array).toBe(true);
     });
 
     test('works with array', async () => {
-        if (!process.env.FIREWORKS_API_KEY) return;
         const embeddingGenerator = new FireworksEmbeddingGenerator();
         const app = mockApp();
         app.settings[EMBEDDING_API_KEY_SETTING] = process.env.FIREWORKS_API_KEY;
         const result = await embeddingGenerator.generateEmbedding(app, ["Hello", "World"], 'query');
         expect(result.length).toBe(2);
         expect(Array.isArray(result)).toBe(true);
-        expect(Array.isArray(result[0])).toBe(true);
-        expect(Array.isArray(result[1])).toBe(true);
+        expect(result[0] instanceof Float32Array).toBe(true);
+        expect(result[1] instanceof Float32Array).toBe(true);
     });
 
     test('throws error with invalid API key', async () => {
